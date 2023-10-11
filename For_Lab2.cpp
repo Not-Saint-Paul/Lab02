7616cc5b96d986aff77a28bc7775a51c9a625bfd
Not-Saint-Paul <145997767+Not-Saint-Paul@users.noreply.github.com>
2023-10-11 16:47:46 +0700

    Update For_Lab2.cpp

--- For_Lab2.cpp
+++ For_Lab2.cpp
@@ -1,8 +1,8 @@
 /************************************
  * Автор: Станковский П.А.          *
  * Дата: 03.10.2023                 *
- * Название: Shitcode Lab2 v2       *
- * https://onlinegdb.com/21DrvpiX0_ *
+ * Название: Shitcode Lab2 v2.1     *
+ * https://onlinegdb.com/0oT0QHRGK  *
  ************************************/
 #include <iostream>
 #include <math.h>
@@ -11,17 +11,23 @@ using namespace std;
 int main() {
     double E = 210, qoefPuas = 0.33, L = 50, dNull = 9.6, d = 1, k;
 
+    while (d < 7) {
+        k = (M_PI * E * (pow(dNull, 4) - pow(d, 4)))/(55.5 * (1 + qoefPuas) * L);
+
+        cout.precision(7);
+        cout << "d = " << d << "  k = " << k << endl;
+
+        d += 2;
+    }
+
     do {
         k = (M_PI * E * (pow(dNull, 4) - pow(d, 4)))/(55.5 * (1 + qoefPuas) * L);
 
         cout.precision(7);
         cout << "d = " << d << "  k = " << k << endl;
 
-        if (d < 7) {
-            d += 2;
-        } else {
-            d += 0.5;
-        }  } while (d <= 9.5)
+        d += 0.5;
+        } while (d <= 9.5)
 
     return 0;
 }
